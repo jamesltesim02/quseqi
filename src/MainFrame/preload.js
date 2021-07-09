04219fc1695a8d42613bc48e7337d42dcf24a89d
Elon <connor@nbmm.co>
2021-07-09 18:29:24 +0800

    获取像素色值

--- src/MainFrame/preload.js
+++ src/MainFrame/preload.js
@@ -1,13 +1,19 @@
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const { ipcRenderer } = require('electron');
 const screenshot = require('screenshot-desktop');
 
-const filepath = path.resolve(__dirname, 'screenshot.png');
+// const filepath = path.resolve(__dirname, 'screenshot.png');
 // const screenshot = require('desktop-screenshot');
 
 window.addEventListener('DOMContentLoaded', () => {
   const screenshotImage = document.getElementById('screenshotImage');
+  const locationText = document.getElementById('locationText');
+  const imageCanvas = document.createElement('canvas');
+  const imageStyle = window.getComputedStyle(screenshotImage);
+  imageCanvas.width = imageStyle.width;
+  imageCanvas.height = imageStyle.height;
+  const imageConext = imageCanvas.getContext('2d');
 
   const toggleFullscreen = fullscreen => {
     if (window.__QUSEQI_FULLSCREEN__ === fullscreen) {
@@ -31,7 +37,20 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   document.body.addEventListener('mousemove', (event) => {
-    console.log(event);
+    if (!window.__QUSEQI_FULLSCREEN__) {
+      return;
+    }
+
+    const { clientX, clientY } = event;
+    locationText.style.left = `${clientX + 10}px`;
+    locationText.style.top = `${clientY}px`;
+    locationText.innerHTML = (
+      `
+      <header>当前坐标:</header>
+      <section>x:${clientX},y:${clientY}</section>
+      <section>${String(imageConext.getImageData(clientX, clientY, 1, 1).data)}</section>
+      `
+    );
   });
 
   document.getElementById('event-trigger').addEventListener('click', async () => {
@@ -77,9 +96,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const imgs = await screenshot.all();
     const base64Data = imgs[0].toString('base64');
-    window.__QUSEQI_SCREENSHOT__ = base64Data;
-
     screenshotImage.src = `data:image/png;base64, ${base64Data}`;
+    imageConext.drawImage(screenshotImage, 0, 0);
+
     toggleFullscreen(true);
   });
 })
